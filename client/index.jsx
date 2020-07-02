a4785167f62d6db432573c6950f22c0381d0a60a
smashingtonn <mitchell.bartlett22@gmail.com>
2020-07-02 13:50:30 +1200

    Change selection type

--- client/index.jsx
+++ client/index.jsx
@@ -13,7 +13,7 @@ import './index.css'
 window.AudioContext = window.AudioContext || window.webkitAudioContext
 
 const ROOM_NAME = window.location.pathname.replace('/', '') || ''
-const HOST = location.href.replace('/^http/', 'ws')
+const HOST = location.href.replace('http', 'ws')
 const store = configureStore()
 const actions = bindActionCreators(Actions, store.dispatch)
 const connector = new Connector(HOST, actions, store)
