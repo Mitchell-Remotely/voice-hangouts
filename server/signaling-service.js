9113ddbec434d8b05d54c5620eabc6a72ca032a2
smashingtonn <mitchell.bartlett22@gmail.com>
2020-07-20 11:00:00 +1200

    Too much

--- server/signaling-service.js
+++ server/signaling-service.js
@@ -30,7 +30,7 @@ class SignalingService {
 
   broadcastToRoomPeers (type, ws, payload, includeSelf) {
     this.wsClients.forEach((wsClient) => {
-      console.log("Room ", wsClient.roomName , ws.roomName, includeSelf, wsClient, ws, payload, type);
+      console.log("Room ", wsClient.roomName , ws.roomName, includeSelf, wsClient.uid, ws.uid, payload, type);
       if (wsClient.roomName === ws.roomName && (includeSelf || wsClient.uid !== ws.uid)) {
         this.send(wsClient, {
           type,
