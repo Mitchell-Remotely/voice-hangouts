ed5389f5f365a46ec476990f72ced7c0ea9ed5ca
smashingtonn <mitchell.bartlett22@gmail.com>
2020-08-19 14:59:30 +1200

    chmod right file

--- startupDev.sh
+++ startupDev.sh
@@ -1,6 +1,6 @@
 sudo git reset --hard origin/development
 sudo git pull
-sudo chmod 777 ./startup.sh
+sudo chmod 777 ./startupDev.sh
 sudo npm install
 sudo npm run build
 sudo rsync -a public/ /var/www/html/
