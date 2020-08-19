5ba5616e94efded5ab556383336d94b3384e9601
smashingtonn <mitchell.bartlett22@gmail.com>
2020-08-19 14:59:01 +1200

    Correct Room to look at

--- client/components/Room/Room.jsx
+++ client/components/Room/Room.jsx
@@ -89,7 +89,7 @@ function Room ({
 
   return (
     <div className={styles.room}>
-      <iframe id="room" src="https://remotely-meeting-rooms.azurefd.net/index.html" className={styles.iframe}></iframe>
+      <iframe id="room" src="https://remotely-voice-server.australiaeast.cloudapp.azure.com/game/" className={styles.iframe}></iframe>
       <div className={styles.userList}>
         {users.map(({ uid, userName, stream, mute }) => (
           <div key={uid} className={styles.userListRow}>
