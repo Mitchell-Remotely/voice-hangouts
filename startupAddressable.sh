a28b1fb94072b9f69c43a89b6d8d444aff9d21f4
smashingtonn <mitchell.bartlett22@gmail.com>
2020-10-29 11:10:58 +1300

    add addressable versions

---
+++ startupAddressable.sh
@@ -0,0 +1,7 @@
+sudo git reset --hard origin/addressables
+sudo git pull
+sudo chmod 777 ./startupAddressable.sh
+sudo npm install
+sudo npm run build
+sudo rsync -a public/ /var/www/html/
+sudo npm run start-addressable
