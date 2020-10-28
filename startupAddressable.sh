f651267e738b14d7eab012683aa6d582747ba689
smashingtonn <mitchell.bartlett22@gmail.com>
2020-10-29 11:45:50 +1300

    Fix addressables

--- startupAddressable.sh
+++ startupAddressable.sh
@@ -2,6 +2,6 @@ sudo git reset --hard origin/addressables
 sudo git pull
 sudo chmod 777 ./startupAddressable.sh
 sudo npm install
-sudo npm run build
+sudo npm run build-addressables
 sudo rsync -a public/ /var/www/html/
 sudo npm run start-addressable
